4ec38161864447a9e68a89be2f4d7685b3ced541
bhardwajvishal0893 <bhardwajvishal0893@gmail.com>
2026-06-11 13:35:29 +0500

    Update User.java

--- src/main/java/com/vishal/notification/entity/User.java
+++ src/main/java/com/vishal/notification/entity/User.java
@@ -15,7 +15,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
+public class User implements java.io.Serializable{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
